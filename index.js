16aad8e964f838f81d110c2340eeb42dc0d2015f
github username <github email address>
2021-04-22 20:32:45 -0400

    commit tree 7317a3eabe19898bb3549ea51190281f7b26ce84

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.startsWith(string))
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string)
+}
